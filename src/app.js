c3da4ec89e1a18c2d8916e4d69a72f47728d34e9
Nader_Arbabian <Nader.arb72@gmail.com>
2019-02-06 22:39:01 -0500

    rewrite random generator

--- src/app.js
+++ src/app.js
@@ -1,2 +1,41 @@
 import Ship from './ship'
 import Surface from './surface'
+
+document.addEventListener("DOMContentLoaded",() => {
+  const canvasEl = document.getElementsByTagName('canvas')[0]
+  // const width = 3500
+  // const height = 550
+  const width = 1440
+  const height = 600
+
+  canvasEl.width = width
+  canvasEl.height = height
+  // canvasEl.width = window.innerWidth
+  
+  //-----CANVAS
+  const ctx = canvasEl.getContext("2d")
+  ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = 'black';
+  //-----CANVAS
+  
+  const surface = new Surface({width, height, gravity:9.8, ctx})
+
+  //----Draw Stars
+    surface.grid.forEach((row,y)=>{
+      row.forEach((el,x)=>{
+        if (el.star === true){
+          ctx.beginPath();
+          ctx.arc(x, y, 1, 0, 2 * Math.PI);
+          ctx.fillStyle = "white";
+          ctx.fill();
+        }
+      })
+    })
+  //----Draw Stars
+
+
+  //--Draw the lines
+    surface.draw()
+
+  //--Draw the lines
+})
--- src/surface.js
+++ src/surface.js
@@ -5,6 +5,10 @@ export default class Surface {
     this.width = options.width
     this.height = options.height
     this.gravity = options.gravity
+    this.generateGrid()
+    this.ctx = options.ctx
+    this.ceiling = this.height * 0.3
+    this.floor = this.height * 0.9
   }
 
   generateGrid(){
@@ -22,236 +26,57 @@ export default class Surface {
   insertStars(){
     this.grid.forEach(row => {
       row.forEach(point => {
-        if (Math.floor(Math.random() * 20) === 1){
+        if (Math.floor(Math.random() * 7000) === 1){
           point.star = true
         }
       })
     })
   }
 
-  generateHill(startPoint,endPoint, peak){
-    let startingXcoord = startPoint[0]
-    let endingXcoord = endPoint
-    let range = endingXcoord - startingXcoord
-    let midPoint = Math.floor((startingXcoord + endingXcoord) / 2)
-    let startingYcoord = startPoint[1]
-    let yAxis
-    while (startingXcoord !== midPoint){
-      let dxdr = ((startingXcoord - midPoint) / range)
-      let modifier = (0.9 + dxdr) * 100 // more modifiers here
-      
-      const flatnessModifier = 100 //this can be changed for steepness
-      //up or down
-      if (Math.floor(Math.random() * flatnessModifier) < modifier) {
-        //up
-        startingYcoord++
-        if (startingYcoord < peak) {
-          this.pointOn(startingXcoord,startingYcoord).ground = true
-        } else { //hit the peak here
-          startingYcoord--
-          this.pointOn(startingXcoord,startingYcoord).ground = true
-        }
-      } 
-      
-      else {
-        //down
-        startingYcoord--
-        if (startingYcoord < peak) {
-          this.pointOn(startingXcoord,startingYcoord).ground = true
-        }
+  draw(){
+
+    let oldX = 0
+    let oldY = this.floor
+    let newX = 1
+    let newY = oldY-1
+
+    while(newX < this.width){
+      // debugger
+      this.drawToCoords([oldX, oldY], [newX, newY])
+      oldX = newX ; oldY = newY
+      newY = newY + this.randomNum(40)
+      while (newY > this.floor || newY < this.ceiling){
+        newY = newY + this.randomNum(10)
       }
-      startingXcoord++
+      newX= newX + 5
     }
 
-    while(startingXcoord !== endingXcoord){
-      let dxdr = ((startingXcoord - endingXcoord) / range)
-      let modifier = (1 + dxdr) * 100
 
-      const flatnessModifier = 120 //this can be changed for steepness
-      if (Math.floor(Math.random() * flatnessModifier) < modifier) {
-        startingYcoord--
-        if (startingYcoord < peak) {
-          this.pointOn(startingXcoord, startingYcoord).ground = true
-          yAxis = startingYcoord
-        }
-      } else {
-        startingYcoord++
-        if (startingYcoord < peak) {
-          this.pointOn(startingXcoord, startingYcoord).ground = true
-          yAxis = startingYcoord
-        } else {
-          startingYcoord--
-          this.pointOn(startingXcoord, startingYcoord).ground = true
-          yAxis = startingYcoord
-        }
-      }
-      startingXcoord++
-    }
-
-    return yAxis
+    // this.drawToCoords([0,this.floor], [500,this.ceiling])
   }
-
-  generateValley(startPoint, endPoint) {
-    //startpoint is coordinate, endpoint is only x axis
-    let startingXcoord = startPoint[0]
-    let endingXcoord = endPoint
-    let range = endingXcoord - startingXcoord
-    let midPoint = Math.floor((startingXcoord + endingXcoord) / 2)
-    let startingYcoord = startPoint[1]
-    let yAxis
-    while (startingXcoord !== midPoint) {
-      let dxdr = ((startingXcoord - midPoint) / range)
-      let modifier = (0.9 + dxdr) * 100 // more modifiers here
-
-      const flatnessModifier = 100 //this can be changed for steepness
-      //up or down
-      if (Math.floor(Math.random() * flatnessModifier) < modifier) {
-        //up
-        startingYcoord++
-        if (startingYcoord > 1) {
-          this.pointOn(startingXcoord, startingYcoord).ground = true
-        } else { //hit the depth here
-          startingYcoord--
-          this.pointOn(startingXcoord, startingYcoord).ground = true
-        }
-      } else {
-        //down
-        startingYcoord--
-        if (startingYcoord > 1) {
-          this.pointOn(startingXcoord, startingYcoord).ground = true
-        }
-      }
-      startingXcoord++
-    }
-
-    while (startingXcoord !== endingXcoord) {
-      let dxdr = ((startingXcoord - endingXcoord) / range)
-      let modifier = (1 + dxdr) * 100
-
-      const flatnessModifier = 120 //this can be changed for steepness
-      if (Math.floor(Math.random() * flatnessModifier) < modifier) {
-        startingYcoord--
-        if (startingYcoord > 1) {
-          this.pointOn(startingXcoord, startingYcoord).ground = true
-          yAxis = startingYcoord
-        }
-      } else {
-        startingYcoord++
-        if (startingYcoord > 1) {
-          this.pointOn(startingXcoord, startingYcoord).ground = true
-          yAxis = startingYcoord
-        } else {
-          startingYcoord--
-          this.pointOn(startingXcoord, startingYcoord).ground = true
-          yAxis = startingYcoord
-        }
-      }
-      startingXcoord++
-    }
-    return yAxis
+  randomNum(max){
+    let num = Math.floor(Math.random() * max) + 1
+    num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1
+    return num
   }
-
-  generateFlat(startPoint, endPoint) {
-    let startingXcoord = startPoint[0]
-    let endingXcoord = endPoint
-    let startingYcoord = startPoint[1]
-    let yAxis
-    while (startingXcoord !== endingXcoord){
-      this.pointOn(startingXcoord, startingYcoord).ground = true
-      this.pointOn(startingXcoord, startingYcoord).bonus = true
-      yAxis = startingYcoord
-      startingXcoord++
-    }
-
-    return yAxis
-  }
-
-  generateBonusFlat(startPoint, endPoint) {
-    let bonus = Math.floor(Math.random() * 6)
-    let startingXcoord = startPoint[0]
-    let endingXcoord = endPoint
-    let startingYcoord = startPoint[1]
-    let yAxis
-    while (startingXcoord !== endingXcoord) {
-      this.pointOn(startingXcoord, startingYcoord).ground = true
-      this.pointOn(startingXcoord, startingYcoord).bonus = bonus
-      yAxis = startingYcoord
-      startingXcoord++
-    }
-    return yAxis
-  }
-
   pointOn(x,y){
     return this.grid[y][x]
   }
 
-  shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-  }
+  drawToCoords(from,to){
+    // debugger
+    const ctx = this.ctx
+    const fromX = from[0]
+    const fromY = from[1]
+    const toX= to[0]
+    const toY= to[1]
 
-  generateRandomSurface(){
-    this.generateGrid()
-    let fields = []
-    let totalPoints = 0
-    
-    for (let i = 0; i < 5; i++) {
-      let rand = Math.floor(Math.random() * 3) + 1
-      fields.push({func: this.generateFlat,num: rand})
-      totalPoints += rand
-    }
-    for (let i = 0; i < 5; i++) {
-      let rand = Math.floor(Math.random() * 3) + 1
-      fields.push({func: this.generateBonusFlat,num: rand})
-      totalPoints += rand
-    }
-
-    for (let i = 0; i < 10; i++) {
-      let rand = Math.floor(Math.random() * 30) + 1
-      fields.push({func: this.generateHill,num: rand})
-      totalPoints += rand
-    }
-
-    for (let i = 0; i < 10; i++) {
-      let rand = Math.floor(Math.random() * 30) + 1
-      fields.push({func: this.generateValley,num: rand})
-      totalPoints += rand
-    }
-    fields = this.shuffle(fields)
-    let multiplier = this.width / totalPoints
-    fields.forEach(touple => {
-      touple.num = Math.ceil(touple.num * multiplier)
-    });
-
-    let xAxis = 0
-    let yAxis = Math.floor(this.height / 2)
-    let that = this
-    window.fields = fields
-    fields.forEach(touple => {
-        if (xAxis + touple.num > this.width){return}
-        yAxis = touple.func.call(that, [xAxis, yAxis], (xAxis + touple.num),(0.8 *this.height))
-        xAxis = xAxis+touple.num
-    });
+    ctx.beginPath();
+    ctx.strokeStyle = "white"
+    ctx.lineWidth = 0.5
+    ctx.moveTo(fromX, fromY)
+    ctx.lineTo(toX, toY);
+    ctx.stroke();
   }
 
 }
-
-const options = {width: 1440,height: 900, gravity: 9.8}
-let surface = new Surface(options)
-surface.generateRandomSurface()
-// surface.generateGrid()
-// surface.generateValley([0,50],20)
-let test = surface.grid.map(row => {
-  return row.map(el => {
-    if (el.ground === true){
-      return "Poi"
-    }
-    return el
-  })
-})
-test = test.reverse()
-debugger
-
